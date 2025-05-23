aa7dc27062ac5194c99be8dcd319f39afd735282
Jared Lemler <jared@jaredlemler.com>
2025-05-22 22:52:56 -0400

    Properly load .env, fix rogue '==' bug, verify Docker Postgres credentials, and survive near-death debugging experience

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 from app.database import SessionLocal
 from sqlalchemy.orm import Session
 from typing import Any
+from app.routers import wishlist
 
 app = FastAPI()
 
 @app.get('/')
 def read_root():
     return {'message': 'Welcome to the Travel Wishlist API!'}
+
+app.include_router(wishlist.router)
--- app/services/wishlist_services.py
+++ app/services/wishlist_services.py
@@ -1,3 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models import WishlistItem
 
+def get_all_items(db: Session):
+    return db.query(WishlistItem).order_by(WishlistItem.added_on.desc()).all()
+
