9f461096afb51f97d9f8719ca4ac4a66f6d59940
Jared Lemler <jared@jaredlemler.com>
2025-06-07 08:52:51 -0400

    update logging

--- app/utils/datadog_logger.py
+++ app/utils/datadog_logger.py
@@ -1,21 +1,10 @@
 # app/utils/datadog_logger.py
 
-import os
-from ddtrace import patch_all, tracer
+from ddtrace import patch_all
 from ddtrace.contrib.asgi import TraceMiddleware
 
-# 🟢 Patch all supported libraries (like SQLAlchemy, etc.)
+# 🟢 Patch all supported libraries (SQLAlchemy, requests, etc.)
 patch_all()
 
-# 🟢 Datadog API key (ensure this is in Railway env)
-DATADOG_API_KEY = os.getenv("DD_API_KEY")
-if not DATADOG_API_KEY:
-    raise Exception("Missing Datadog API Key!")
-
-# 🟢 Set up tracer (Datadog uses this to send traces)
-tracer.configure(
-    hostname="https://trace.agent.datadoghq.com",  # Datadog APM endpoint
-)
-
-# 🟢 Use this middleware in your FastAPI app (see below)
+# 🟢 Provide middleware to be added in main.py
 datadog_middleware = TraceMiddleware
